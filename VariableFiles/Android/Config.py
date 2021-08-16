3e9a1f29c7d4b597357a1f8bf3cb7611205ec0fb
afsndrdtv <afsndrdtv@gmail.com>
2021-08-16 16:19:57 +0530

    adding variable file

---
+++ VariableFiles/Android/Config.py
@@ -0,0 +1,21 @@
+import os
+
+APPIUM_SERVER = 'http://127.0.0.1:4723/wd/hub'
+
+TEST_DEVICE= '192.168.1.5:5555'
+TEST_DEVICE_PHONENUMBER= ''
+PARTNER_DEVICE= '192.168.1.3:5555'
+PARTNER_DEVICE_PHONENUMBER= ''
+
+CHROME_PORT = 8300
+SYSTEM_PORT = 8400
+MJPEG_PORT = 8500
+
+APP_PACKAGE = 'com.onemainstream.sonyliv.android'
+APP_ACTIVITY = 'com.sonyliv.ui.splash.SplashActivity'
+
+APP_PACKAGE_FIREFOX = 'org.mozilla.tv.firefox'
+
+hidden = 'hidden india'
+
+PIN = 1111
